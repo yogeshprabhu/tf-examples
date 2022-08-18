0695d05b43a64ab51e15e8ea692a968aaa395193
yogeshprabhu <yogesh.prabhu@gmail.com>
2022-08-18 10:05:23 -0400

    formatted

--- main.tf
+++ main.tf
@@ -1,70 +1,33 @@
 terraform {
-
   cloud {
-
     organization = "YoHashi "
-
-
-
     workspaces {
-
       name = "tf-examples"
-
     }
-
   }
-
 }
-
-
-
 terraform {
   required_providers {
     tfe = {
       source = "hashicorp/tfe"
       version = "0.36.0"
     }
-
   }
-
 }
-
-
-
 data "tfe_organization" "foo" {
-
   name = "YoHashi"
-
 }
-
-
-
 output "org" {
-
   description = "Org Name"
-
   value = data.tfe_organization.foo.name
-
 }
-
-
-
 output "email" {
-
   description = "Org Email"
-
   value = data.tfe_organization.foo.email
-
 }
-
-
-
 resource "tfe_variable_set" "test" {
-
   name = "Test Varset"
-
   description = "Some description."
-
   organization = data.tfe_organization.foo.name
 
 }
